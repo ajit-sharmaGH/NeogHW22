2481f73ad40c2cfc4dc4bc7248dd4db35a047eee
AJIT SHARMA <ajit08082000@gmail.com>
2022-01-15 11:58:54 +0530

    added a code

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-var val = 3;
+ var val = 3;
 
-function Name() {
+ function Name() {
 
 
-    var val = 10;
-    console.log(val);
-}
-Name();
 
-const obj = {
-    name: "Ajit",
-    lastName: "Sharma "
-}
-console.log(`${obj.name + obj.lastName}`);
+
+     var val = 10;
+     console.log(val);
+ }
+ Name();
+
+ const obj = {
+     name: "Ajit",
+     lastName: "Sharma "
+ };
+ console.log(`${obj.name + obj.lastName}`);
+ //nullish coalescing operator and logical || (or) operator
+
+ function chargeAmount() {
+     return 10;
+ }
+
+
+ let message = chargeAmount() || "we are here to learn";
+ console.log(message);
