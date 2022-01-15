6827373ad995e9589045fccee9e2d69a92832eff
AJIT SHARMA <ajit08082000@gmail.com>
2022-01-15 11:36:48 +0530

    hi

---
+++ script.js
@@ -0,0 +1,15 @@
+var val = 3;
+
+function Name() {
+
+
+    var val = 10;
+    console.log(val);
+}
+Name();
+
+const obj = {
+    name: "Ajit",
+    lastName: "Sharma "
+}
+console.log(`${obj.name + obj.lastName}`);
